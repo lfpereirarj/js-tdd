579025bb555548b5af684f3a03a73e7abea79a58
Luiz Felipe <lf.pereirarj@gmail.com>
2018-05-28 22:21:28 -0300

    convertendo para es6

--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
 
-const sum = (num1, num2) => num1 + num2;
-const sub = (num1, num2) => num1 - num2;
-const mult = (num1, num2) => num1 * num2;
-const div = (num1, num2) => (num2 === 0) ? 'Não é possível dividir por zero!' : num1 / num2;
-
-export { sum, sub, mult, div }
