571a413221bf0a3759c24e8a81ffa7c26e293ad9
Luiz Felipe <lf.pereirarj@gmail.com>
2018-05-29 22:01:39 -0300

    fizzbuzz

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,6 +1,25 @@
 import { expect } from 'chai';
+import FizzBuzz from '../src/main';
 
 
 describe('Main', () => {
+
+  it('should return `Fizz` when multiple of 3', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
+  })
+
+  it('should return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+    expect(FizzBuzz(25)).to.be.equal('Buzz');
+  })
+
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+  })
+
+  it('should return number when multiple of 3', () => {
+    expect(FizzBuzz(4)).to.be.equal(4);
+  })
 
 });
